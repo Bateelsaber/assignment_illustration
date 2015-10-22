a8d09bbfd2c47b9ad0711f920ead7360b2d29c33
Bateelsaber <sabeb234@newschool.edu>
2015-10-22 00:52:11 -0400

    load test images and set up codes

--- illustration_p5/illustration.js
+++ illustration_p5/illustration.js
@@ -1,18 +1,65 @@
 // illustration.js
 
-var backgroundImg1;
-var backgroundImg2;
-var pigeon1;
-var pigeon2;
-var pigeonpoo;
-var poo;
-var watersplash;
-var birds
+var backgroundImg;
+var pigeonImg;
+var pigeonpooImg;
+var pooImg;
+var watersplashImg;
+var birdsImg;
+
+var pigeonX = -100
+var birdsX = -300
+var pooY = 130
+
+function preload() {
+	backgroundImg = loadImage("background1.png");
+	pigeonImg = loadImage("pigeon1.jpg");
+	pigeonpooImg = loadImage("pigeonpoopose.jpg");
+	pooImg = loadImage("poo.jpg");
+	watersplashImg = loadImage("watersplash.jpg");
+	birdsImg = loadImage("birds.jpg");
+}
+
 
 function setup() {
 	createCanvas(800, 600);
 }
 
 function draw() {
-	background(200, 0, 0);
+	image(backgroundImg, 0, 0);
+
+	//animate pigeon & birds
+	birdsX += 0.6;
+	if (birdsX > 665) {
+		birdsX = -10;
+	}
+	image(birdsImg, birdsX, 120);
+
+	pigeonX += 1;
+	if (pigeonX > 607) {
+		pigeonX = -100;
+	}
+	image(pigeonImg, pigeonX, 100);
+	if (mouseIsPressed) {
+		image(pigeonpooImg, pigeonX, 100);
+	}
+
+
+
+	var pooX;
+	var watertouch = 396
+
+
+	if (mouseIsPressed) {
+		pooX = mouseX;
+		pooY += 5;
+		if (pooY > 430) {
+			pooY = 120;
+		}
+	}
+	image(pooImg, pigeonX + 45, pooY);
+
+	if (pooY > 385) {
+		image(watersplashImg, pigeonX, watertouch);
+	}
 }
