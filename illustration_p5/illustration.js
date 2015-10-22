85648601f60e2fa18c5413c5a4e653628b863263
Bateelsaber <sabeb234@newschool.edu>
2015-10-21 22:26:42 -0400

    setting variables

--- illustration_p5/illustration.js
+++ illustration_p5/illustration.js
@@ -1,9 +1,18 @@
 // illustration.js
 
+var backgroundImg1;
+var backgroundImg2;
+var pigeon1;
+var pigeon2;
+var pigeonpoo;
+var poo;
+var watersplash;
+var birds
+
 function setup() {
 	createCanvas(800, 600);
 }
 
 function draw() {
-	background(200,0,0);
+	background(200, 0, 0);
 }
